a0d0a22863767f00f2de88d00cc1a1f0086d3e23
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-04 23:15:30 +0600

    services commit

--- src/Components/AllServices/AllServices.js
+++ src/Components/AllServices/AllServices.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Container } from "react-bootstrap";
 import "./allServices.css";
-import firstImg from "../../Group 1372.png";
-import MouthImg from "../../Mouth.png";
-import FaceImg from "../../face.png";
 import Button from "@mui/material/Button";
 const AllServices = () => {
+    const [service, setService] = useState([]);
+    const [handleBtn, setHandleBtn] = useState(false);
+    const [trueService, setTrueService] = useState([]);
+
+    useEffect(() => {
+        fetch("./services.json")
+            .then((res) => res.json())
+            .then((data) => {
+                if (handleBtn === false) {
+                    const slicingData = data.slice(0, 3);
+                    setService(slicingData);
+                } else {
+                    setTrueService(data);
+                }
+            });
+    }, []);
+    console.log(trueService);
+    const handleExploreMore = () => {
+        setHandleBtn(true);
+    };
     return (
         <Container fluid>
             <Container>
@@ -22,136 +39,170 @@ const AllServices = () => {
                 </div>
                 <div className="main-services">
                     <div className="all-services">
-                        <div>
-                            <Card style={{ width: "22rem" }}>
-                                <img
-                                    src={firstImg}
-                                    className="img-fluid"
-                                    style={{
-                                        width: "7rem",
-                                        marginLeft: "auto",
-                                        marginRight: "auto",
-                                        marginTop: "30px",
-                                    }}
-                                />
-                                <Card.Body>
-                                    <Card.Title
-                                        style={{ fontWeight: "bold" }}
-                                        className="text-center"
-                                    >
-                                        Anti Age Face Treatment
-                                    </Card.Title>
-                                    <p
-                                        className="text-center"
+                        <div
+                            style={{
+                                display: "grid",
+                                gridTemplateColumns: "repeat(3, 1fr)",
+                                gridGap: "50px",
+                            }}
+                            className="all-services-cards ms-auto mt-5 mb-5"
+                        >
+                            {handleBtn ? (
+                                <div>
+                                    {trueService.map((service) => (
+                                        <Card
+                                            style={{ width: "22rem" }}
+                                            key={service.title}
+                                            className="a-card"
+                                        >
+                                            <img
+                                                src={service.image}
+                                                className="img-fluid"
+                                                style={{
+                                                    width: "7rem",
+                                                    marginLeft: "auto",
+                                                    marginRight: "auto",
+                                                    marginTop: "30px",
+                                                }}
+                                            />
+                                            <Card.Body>
+                                                <Card.Title
+                                                    style={{
+                                                        fontWeight: "bold",
+                                                    }}
+                                                    className="text-center"
+                                                >
+                                                    {service.title}
+                                                </Card.Title>
+                                                <p
+                                                    className="text-center"
+                                                    style={{
+                                                        color: "#f63e7b",
+                                                        fontWeight: "bold",
+                                                    }}
+                                                >
+                                                    $
+                                                    <span>{service.price}</span>
+                                                </p>
+                                                <Card.Text className="lead">
+                                                    {service.description}
+                                                </Card.Text>
+                                                <Button
+                                                    color="secondary"
+                                                    variant="outlined"
+                                                >
+                                                    Know More
+                                                </Button>
+                                            </Card.Body>
+                                        </Card>
+                                    ))}
+                                </div>
+                            ) : (
+                                <div>
+                                    {service.map((t) => (
+                                        <Card
+                                            style={{ width: "22rem" }}
+                                            key={t.title}
+                                            className="a-card"
+                                        >
+                                            <img
+                                                src={t.image}
+                                                className="img-fluid"
+                                                style={{
+                                                    width: "7rem",
+                                                    marginLeft: "auto",
+                                                    marginRight: "auto",
+                                                    marginTop: "30px",
+                                                }}
+                                            />
+                                            <Card.Body>
+                                                <Card.Title
+                                                    style={{
+                                                        fontWeight: "bold",
+                                                    }}
+                                                    className="text-center"
+                                                >
+                                                    {t.title}
+                                                </Card.Title>
+                                                <p
+                                                    className="text-center"
+                                                    style={{
+                                                        color: "#f63e7b",
+                                                        fontWeight: "bold",
+                                                    }}
+                                                >
+                                                    $<span>{t.price}</span>
+                                                </p>
+                                                <Card.Text className="lead">
+                                                    {t.description}
+                                                </Card.Text>
+                                                <Button
+                                                    color="secondary"
+                                                    variant="outlined"
+                                                >
+                                                    Know More
+                                                </Button>
+                                            </Card.Body>
+                                        </Card>
+                                    ))}
+                                </div>
+                            )}
+                            {/* {service.map((service) => (
+                                <Card
+                                    style={{ width: "22rem" }}
+                                    key={service.title}
+                                    className="a-card"
+                                >
+                                    <img
+                                        src={service.image}
+                                        className="img-fluid"
                                         style={{
-                                            color: "#f63e7b",
-                                            fontWeight: "bold",
+                                            width: "7rem",
+                                            marginLeft: "auto",
+                                            marginRight: "auto",
+                                            marginTop: "30px",
                                         }}
-                                    >
-                                        $199
-                                    </p>
-                                    <Card.Text className="lead">
-                                        We craft stunning and amazing web UI
-                                        using a well dramatic UX to fit your
-                                        product.
-                                    </Card.Text>
-                                    <Button
-                                        color="secondary"
-                                        variant="outlined"
-                                    >
-                                        Know More
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <div>
-                            <Card style={{ width: "22rem" }}>
-                                <Card.Img
-                                    variant="top"
-                                    src={MouthImg}
-                                    className="img-fluid"
-                                    style={{
-                                        width: "7rem",
-                                        marginLeft: "auto",
-                                        marginRight: "auto",
-                                        marginTop: "30px",
-                                    }}
-                                />
-                                <Card.Body>
-                                    <Card.Title
-                                        style={{ fontWeight: "bold" }}
-                                        className="text-center"
-                                    >
-                                        Hair Color & Styling
-                                    </Card.Title>
-                                    <p
-                                        className="text-center"
-                                        style={{
-                                            color: "#f63e7b",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        $99
-                                    </p>
-                                    <Card.Text className="lead">
-                                        Amazing flyers, social media posts and
-                                        brand representations that would make
-                                        your brand stand out.
-                                    </Card.Text>
-                                    <Button
-                                        color="secondary"
-                                        variant="outlined"
-                                    >
-                                        Know More
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <div>
-                            <Card style={{ width: "22rem" }}>
-                                <Card.Img
-                                    variant="top"
-                                    src={FaceImg}
-                                    className="img-fluid"
-                                    style={{
-                                        width: "7rem",
-                                        marginLeft: "auto",
-                                        marginRight: "auto",
-                                        marginTop: "30px",
-                                    }}
-                                />
-                                <Card.Body>
-                                    <Card.Title
-                                        style={{ fontWeight: "bold" }}
-                                        className="text-center"
-                                    >
-                                        Skin Care Treatment
-                                    </Card.Title>
-                                    <p
-                                        className="text-center"
-                                        style={{
-                                            color: "#f63e7b",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        $299
-                                    </p>
-                                    <Card.Text className="lead">
-                                        With well written codes, we build
-                                        amazing apps for all platforms, mobile
-                                        and web apps in general.
-                                    </Card.Text>
-                                    <Button
-                                        color="secondary"
-                                        variant="outlined"
-                                    >
-                                        Know More
-                                    </Button>
-                                </Card.Body>
-                            </Card>
+                                    />
+                                    <Card.Body>
+                                        <Card.Title
+                                            style={{ fontWeight: "bold" }}
+                                            className="text-center"
+                                        >
+                                            {service.title}
+                                        </Card.Title>
+                                        <p
+                                            className="text-center"
+                                            style={{
+                                                color: "#f63e7b",
+                                                fontWeight: "bold",
+                                            }}
+                                        >
+                                            $<span>{service.price}</span>
+                                        </p>
+                                        <Card.Text className="lead">
+                                            {service.description}
+                                        </Card.Text>
+                                        <Button
+                                            color="secondary"
+                                            variant="outlined"
+                                        >
+                                            Know More
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            ))} */}
+                            {/*  */}
                         </div>
                     </div>
                 </div>
+                <Button
+                    variant="outlined"
+                    color="error"
+                    style={{ marginLeft: "45%", marginBottom: "50px" }}
+                    onClick={handleExploreMore}
+                >
+                    Explore More
+                </Button>
             </Container>
         </Container>
     );
