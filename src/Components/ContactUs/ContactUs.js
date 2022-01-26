782123d40e98077d4f9ed6c2a2d78521e507e397
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-26 13:47:42 +0600

    google map uploaded done

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,82 +1,107 @@
 import React from "react";
 import { Container, Form } from "react-bootstrap";
 import "./contactUs.css";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+    width: "400px",
+    height: "400px",
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523,
+};
 const ContactUs = () => {
     return (
-        <Container fluid className="contact-main">
-            <Container>
-                <p
-                    className="text-center"
-                    style={{
-                        color: "white",
-                        fontSize: "1.3rem",
-                        fontFamily: "Sedgwick Ave",
-                    }}
-                >
-                    Welcome To Jerin Parlour
-                </p>
-                <h1
-                    className="text-center"
-                    style={{
-                        color: "white",
-                        fontSize: "3.3rem",
-                        fontFamily: "Sedgwick Ave",
-                    }}
-                >
-                    CONTACT US
-                </h1>
+        <>
+            <Container fluid className="contact-main">
                 <Container>
-                    <>
-                        <Form.Floating className="mb-3">
-                            <Form.Control
-                                id="floatingInputCustom"
-                                type="text"
-                                placeholder="name@example.com"
-                                className="form-email"
-                            />
-                            <label
-                                htmlFor="floatingInputCustom"
-                                style={{ color: "white" }}
-                                className="lead"
-                            >
-                                Name
-                            </label>
-                        </Form.Floating>
-                        <Form.Floating className="mb-3">
-                            <Form.Control
-                                id="floatingPasswordCustom"
-                                type="password"
-                                placeholder="Password"
-                                className="form-email"
-                            />
-                            <label
-                                htmlFor="floatingPasswordCustom"
-                                style={{ color: "white" }}
-                                className="lead"
-                            >
-                                Company
-                            </label>
-                        </Form.Floating>
-                        <Form.Floating>
-                            <Form.Control
-                                id="floatingPasswordCustom"
-                                type="email"
-                                placeholder="Password"
-                                className="form-email"
-                            />
-                            <label
-                                htmlFor="floatingPasswordCustom"
-                                style={{ color: "white" }}
-                                className="lead"
-                            >
-                                Email*
-                            </label>
-                        </Form.Floating>
-                    </>
-                    <button className="btn-submit mt-5 lead">Send</button>
+                    <p
+                        className="text-center"
+                        style={{
+                            color: "white",
+                            fontSize: "1.3rem",
+                            fontFamily: "Sedgwick Ave",
+                        }}
+                    >
+                        Welcome To Jerin Parlour
+                    </p>
+                    <h1
+                        className="text-center"
+                        style={{
+                            color: "white",
+                            fontSize: "3.3rem",
+                            fontFamily: "Sedgwick Ave",
+                        }}
+                    >
+                        CONTACT US
+                    </h1>
+                    <Container>
+                        <>
+                            <Form.Floating className="mb-3">
+                                <Form.Control
+                                    id="floatingInputCustom"
+                                    type="text"
+                                    placeholder="name@example.com"
+                                    className="form-email"
+                                />
+                                <label
+                                    htmlFor="floatingInputCustom"
+                                    style={{ color: "white" }}
+                                    className="lead"
+                                >
+                                    Name
+                                </label>
+                            </Form.Floating>
+                            <Form.Floating className="mb-3">
+                                <Form.Control
+                                    id="floatingPasswordCustom"
+                                    type="password"
+                                    placeholder="Password"
+                                    className="form-email"
+                                />
+                                <label
+                                    htmlFor="floatingPasswordCustom"
+                                    style={{ color: "white" }}
+                                    className="lead"
+                                >
+                                    Company
+                                </label>
+                            </Form.Floating>
+                            <Form.Floating>
+                                <Form.Control
+                                    id="floatingPasswordCustom"
+                                    type="email"
+                                    placeholder="Password"
+                                    className="form-email"
+                                />
+                                <label
+                                    htmlFor="floatingPasswordCustom"
+                                    style={{ color: "white" }}
+                                    className="lead"
+                                >
+                                    Email*
+                                </label>
+                            </Form.Floating>
+                        </>
+                        <button className="btn-submit mt-5 lead">Send</button>
+                    </Container>
                 </Container>
             </Container>
-        </Container>
+            <Container>
+                <LoadScript googleMapsApiKey="AIzaSyD8dMPRyH5KBmH_KfiS8zsu1DaeOtkZFgc">
+                    <GoogleMap
+                        mapContainerStyle={containerStyle}
+                        center={center}
+                        zoom={10}
+                    >
+                        {/* Child components, such as markers, info windows, etc. */}
+                        <></>
+                    </GoogleMap>
+                </LoadScript>
+            </Container>
+        </>
     );
 };
 
